8a1c83829a6f347acc0b73eaaa473a7a16105404
nojimage <nojimage@gmail.com>
2019-04-08 14:00:43 +0900

    use Type::getMap/setMap instead Type::map.
    for CakePHP >= 3.6.2

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -6,7 +6,13 @@
  *
  */
 use Cake\Database\Type;
+use Elastic\ActivityLogger\Database\Type\JsonDataType;
 
-if (!Type::map('json_data')) {
-    Type::map('json_data', '\Elastic\ActivityLogger\Database\Type\JsonDataType');
+// TODO: Drop JsonDataType next release
+if (method_exists(Type::class, 'getMap')) {
+    if (!Type::getMap('json_data')) {
+        Type::setMap(array_merge(Type::getMap(), ['json_data' => JsonDataType::class]));
+    }
+} elseif (!Type::map('json_data')) {
+    Type::map('json_data', JsonDataType::class);
 }
