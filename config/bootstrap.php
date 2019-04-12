87c7e2af6f33d96c4c140ca2581633f3cec03014
nojimage <nojimage@gmail.com>
2019-04-12 13:43:52 +0900

    fixes bootstrap

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -9,10 +9,7 @@ use Cake\Database\Type;
 use Elastic\ActivityLogger\Database\Type\JsonDataType;
 
 // TODO: Drop JsonDataType next release
-if (method_exists(Type::class, 'getMap')) {
-    if (!Type::getMap('json_data')) {
-        Type::setMap(array_merge(Type::getMap(), ['json_data' => JsonDataType::class]));
-    }
-} elseif (!Type::map('json_data')) {
+$getMap = method_exists(Type::class, 'getMap') ? 'getMap' : 'map';
+if (!Type::$getMap('json_data')) {
     Type::map('json_data', JsonDataType::class);
 }
